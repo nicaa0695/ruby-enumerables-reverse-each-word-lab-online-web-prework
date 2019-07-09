ceb4e547d95d0239e9f3039323961ef05064dbcd
Veronica Lopez <nicaa0695@gmail.com>
2019-07-09 00:43:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,7 +6,7 @@ def reverse_each_word(string)
    while(i < string.length)
    string.each do |string|
     string = string.reverse()
-    newString[j] = String 
+    new.String[j] = String 
      i += 1 
      j += 1 
    end
