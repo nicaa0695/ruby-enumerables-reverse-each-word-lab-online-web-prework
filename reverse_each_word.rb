0a9eefaef15b0192d12dd1f70edc3c44fa2a28f5
Veronica Lopez <nicaa0695@gmail.com>
2019-07-09 00:39:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,4 @@
 def reverse_each_word(string)
   string = string.split(" ")
-   i=0 
-   j=0 
-   new_string = [" "]
-   while(i < string.length)
-   string.each do |string|
-    string = string.reverse()
-    newString[j] = string 
-     i += 1 
-     j += 1 
-   end
-    newString = string.collect{|reverse_string| reverse_string.reverse}
-  end
-  return newString.join(' ')
+ return newString.join(' ')
 end
