753a815c8ff3918ccb0ca6253257d113afb3073e
Veronica Lopez <nicaa0695@gmail.com>
2019-07-09 00:34:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,12 @@
 def reverse_each_word(string)
   string = string.split(" ")
+  i=0 
+  j=0 
+  new_string = [" "]
+  while(i < string.length)
+  string.each do |string|
+    string = string.reverse()
+    newString[j] = string 
+    i += 1 
+    j += 1 
 end
